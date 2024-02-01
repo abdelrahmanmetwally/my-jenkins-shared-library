e7cb867d087706956f495117d22225b7fd262fa4
abdelrahman metwally <93939024+abdelrahmanmetwally@users.noreply.github.com>
2024-02-01 06:46:26 +0200

    Update buildAndPush.groovy

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "docker login -u ${config.USERNAME} -p ${config.PASSWORD}"
-        sh "docker build -t ${config.DOCKER_REGISTRY}/${config.DOCKER_IMAGE}:v${config.BUILD_NUMBER} ${config.BUILD_PATH}"
+        sh "docker build -t ${config.DOCKER_REGISTRY}/${config.DOCKER_IMAGE}:v${config.BUILD_NUMBER} ."
         sh "docker push ${config.DOCKER_REGISTRY}/${config.DOCKER_IMAGE}:v${config.BUILD_NUMBER}"
     }
 }
