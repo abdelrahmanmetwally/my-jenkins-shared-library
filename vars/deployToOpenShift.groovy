e6a609bb0c6f49f111c7f28221bec47f26e95ff8
abdelrahman metwally <93939024+abdelrahmanmetwally@users.noreply.github.com>
2024-02-01 08:24:43 +0200

    Update deployToOpenShift.groovy

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
     withCredentials([file(credentialsId: 'openshift-credentials', variable: 'KUBECONFIG')]) {
         sh """
-            oc login --token=${config.OPENSHIFT_TOKEN} --server=${config.SERVER_NAME} --insecure-skip-tls-verify
+            oc login --token=${config.KUBECONFIG} --server=${config.SERVER_NAME} --insecure-skip-tls-verify
             oc project ${config.OPENSHIFT_PROJECT}
             oc apply -f DeployRoute.yml --kubeconfig ${config.KUBECONFIG} -n ${config.OPENSHIFT_PROJECT}
         """
